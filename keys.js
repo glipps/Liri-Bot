aab97fbc9c4f288f3fde11090966c8299d7c04d0
glipps <glipps@kent.edu>
2019-04-04 17:10:21 -0400

    Create keys.js

---
+++ keys.js
@@ -0,0 +1,11 @@
+console.log("API Keys are now loaded");
+
+exports.spotifyKey = {
+    id: "6151c5e78f4443668de8145f196715b1",
+    secret: "94ad15a3eec346e999087d22dc20bf45",
+};
+
+exports.bandsintownKey = {
+    id:"placeholder",
+    secret:"placeholder",
+};
